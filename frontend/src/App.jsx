df9097197d91c9be21510893e4c4ddb2da3f1e95
Abhinav Sreejith <abhinavsree243@gmail.com>
2026-01-19 12:13:09 +0530

    modified routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from 'react-router'
 import HomePage from './pages/HomePage'
 import NoteCreate from './pages/NoteCreate'
 import NoteDetail from './pages/NoteDetail'
+import LandingPage from './pages/LandingPage'
 
 
 const App = () => {
   return (
     <div data-theme="forest">
       <Routes>
-        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/' element={<LandingPage/>} ></Route>
+        <Route path='/home' element={<HomePage />}></Route>
         <Route path='/create' element={<NoteCreate />}></Route>
         <Route path='/note/:id' element={<NoteDetail />}></Route>
       </Routes>
